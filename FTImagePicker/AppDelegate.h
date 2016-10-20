50147830ccac67a845d0176ccd07fb815ffabd41
Park <park@Parkui-MacBook-Pro.local>
2016-10-20 15:00:23 +0900

    Initial Commit

---
+++ FTImagePicker/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  FTImagePicker
+//
+//  Created by Park on 2016. 10. 20..
+//  Copyright © 2016년 Parkfantagram /inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ FTImagePicker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FTImagePicker
+//
+//  Created by Park on 2016. 10. 20..
+//  Copyright © 2016년 Parkfantagram /inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
